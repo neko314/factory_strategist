1b887c5ba9f2f6677ad11be5a4a55c8b4016c4ef
Keiko Kaneko <keiko.cda@gmail.com>
2021-08-28 12:36:16 +0900

    Delete sample spec

--- spec/factory_strategist_spec.rb
+++ spec/factory_strategist_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe FactoryStrategist do
   it "has a version number" do
     expect(FactoryStrategist::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
