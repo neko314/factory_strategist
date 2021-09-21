efb3f99e4d3349752f2d8b11d6a4303d16ca7431
Keiko Kaneko <keiko.cda@gmail.com>
2021-09-21 21:59:57 +0900

    alias_method :create, :build_stubbed

--- lib/factory_strategist.rb
+++ lib/factory_strategist.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
 require_relative "factory_strategist/version"
+require "rspec"
+require "factory_bot"
 
+# Settings to see which method is best
 module FactoryStrategist
-  class Error < StandardError; end
-  # Your code goes here...
+  RSpec.configure do |config|
+    config.before(:suite) do |_ex|
+      FactoryBot::Syntax::Methods.alias_method :create, :build_stubbed
+    end
+
+    config.after(:example) do |ex|
+      case ex.exception
+      when RSpec::Expectations::ExpectationNotMetError
+        # no-op
+      when nil
+        p "#{ex.location} create can be replaced to build_stubbed"
+      end
+    end
+  end
 end
