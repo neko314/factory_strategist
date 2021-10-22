ddd7574cdac8d60d4192ba7318a6374c9717db3d
OKURA Masafumi <masafumi.o1988@gmail.com>
2021-10-22 23:27:14 +0900

    Change env name for enabling this gem
    I think it has typo, and I believe with underscore it looks much nicer.

--- lib/factory_strategist.rb
+++ lib/factory_strategist.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
 require_relative "factory_strategist/version"
-require_relative "factory_strategist/configure" if ENV["FATORYSTRATEGIST"]
+require_relative "factory_strategist/configure" if ENV["FACTORY_STRATEGIST"]
